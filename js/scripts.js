c3b109bd84b910426fb3ce38d35221bdb8960dd8
AwayeCode <98905695+AwayeCode@users.noreply.github.com>
2022-06-10 12:32:30 +0200

    refactoring my code using the forEach() function rather than the regular for loop to iterate over my repository array
    Using a forEach() function instead of the for loop you have to iterate over the Pokémon in my pokemonList array in order to print the details of each one

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-let pokemonList = ["Bulbasaur (height: 0.7)", "Sharpedo (height: 1.7)", "Wailord (height: 1)"];
-let text = "";
-let i = 0;
-for (; pokemonList[i];) {
-    text = text + " " + pokemonList[i];
-    i++;
-}
-document.write(text);
+let pokemonList = [
+  {
+    name: "Bulbasaur",
+    height: 0.7,
+    types: ["grass", "poison"]
+  },
+  {
+    name: "Sharpedo",
+    height: 1.7,
+    types: ["fire", "flying"]
+  }, 
+  {
+    name: "Wailord",
+    height: 1,
+    types: ["water"]
+  }
+]
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + " is" + " " + pokemon.height + " in height" + " and is a type of" + " " + pokemon.types);
+});
 
 
 
