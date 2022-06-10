d9c57494a4974c020242a63113db7d5d70e003c0
AwayeCode <98905695+AwayeCode@users.noreply.github.com>
2022-06-10 19:27:09 +0200

    adding a forEach function to my repository
    A forEach function is applied to iterate over the pokemonRepository in the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,60 @@
 let pokemonList = [
   {
-    name: "Bulbasaur",
+    name: `Bulbasaur`,
     height: 0.7,
-    types: ["grass", "poison"]
+    types: [`grass`, `poison`]
   },
   {
-    name: "Sharpedo",
+    name: `Sharpedo`,
     height: 1.7,
-    types: ["fire", "flying"]
-  }, 
+    types: [`fire`, `flying`]
+  },
   {
-    name: "Wailord",
+    name: `Wailord`,
     height: 1,
-    types: ["water"]
+    types: [`water`]
   }
-]
+];
 
-pokemonList.forEach(function(pokemon) {
+pokemonList.forEach(function (pokemon) {
   document.write(pokemon.name + " is" + " " + pokemon.height + " in height" + " and is a type of" + " " + pokemon.types);
-});
+})
 
 
+let pokemonRepository = (function () {
+  let pokemonList = [{
+    name: `Bulbasaur`,
+    height: 0.7,
+    types: [`grass`, `poison`]
+  },
+  {
+    name: `Sharpedo`,
+    height: 1.7,
+    types: [`fire`, `flying`]
+  },
+  {
+    name: `Wailord`,
+    height: 1,
+    types: [`water`]
+  }]
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+document.write(pokemonRepository.getAll());
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.forEach(function (pokemon) {
+  document.write(pokemonRepository.name + " is" + " " + pokemon.height + " in height" + " and is a type of" + " " + pokemon.types);
+})
 
