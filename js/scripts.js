82d9d922e5f1a8305c093d534b267dc3f5834137
AwayeCode <98905695+AwayeCode@users.noreply.github.com>
2022-06-23 14:29:52 +0200

    adjustment made to js,scripts file
    The code in my js,script file was rewritten to allow my code to be wrapped in IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,46 @@
-let pokemonList = [
-  {
-    name: `Bulbasaur`,
-    height: 0.7,
-    types: [`grass`, `poison`]
-  },
-  {
-    name: `Sharpedo`,
-    height: 1.7,
-    types: [`fire`, `flying`]
-  },
-  {
-    name: `Wailord`,
-    height: 1,
-    types: [`water`]
-  }
-];
 
-pokemonList.forEach(function (pokemon) {
-  document.write(pokemon.name + " is" + " " + pokemon.height + " in height" + " and is a type of" + " " + pokemon.types);
-})
+let pokemonRepository = (function(){ 
+  let pokemonList = [
+    {
+      name: `Bulbasaur`,
+      height: 1.7,
+      type: [`grass`, `poison`]
+    },
+   
+    {
+      name: `Charizard`,
+      height: 0.7,
+      type: [`fire`, `flying`]
+    },
+    {
+      name: `Squirtle`,
+      height: 1,
+      type: [`water`]
+    }
+  ]
+    function myLoopFunction(pokemon){
+      document.write(pokemon.name + " " + "is" + " " + pokemon.height + " " + "in height");
+    document.write("<br>");
+    };
+    pokemonList.forEach(myLoopFunction);
 
-
-let pokemonRepository = (function () {
-  let pokemonList = [{
-    name: `Bulbasaur`,
-    height: 0.7,
-    types: [`grass`, `poison`]
-  },
-  {
-    name: `Sharpedo`,
-    height: 1.7,
-    types: [`fire`, `flying`]
-  },
-  {
-    name: `Wailord`,
-    height: 1,
-    types: [`water`]
-  }]
-
-  function add(pokemon) {
-    pokemonList.push(pokemon);
-  }
-
-  function getAll() {
-    return pokemonList;
-  }
-
-  return {
-    add: add,
-    getAll: getAll
-  };
-})();
-document.write(pokemonRepository.getAll());
-console.log(pokemonRepository.getAll());
-
-pokemonRepository.forEach(function (pokemon) {
-  document.write(pokemonRepository.name + " is" + " " + pokemon.height + " in height" + " and is a type of" + " " + pokemon.types);
-})
-
+    function add(pokemon){
+      pokemonList.push(pokemon);
+    }
+    function getAll(){
+      return pokemonList;
+    }
+    return {
+      add : add,
+      getAll : getAll
+      
+    };
+    })()
+  pokemonRepository.add[{
+    name: `Scyther`,
+    type: [`Bug`, `Flying`],
+    height: 4,
+    abilities: [`Swarm`, `Slash`]
+  }];
+  
+  document.write(pokemonRepository.getAll())
