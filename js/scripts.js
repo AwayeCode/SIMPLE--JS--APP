815183913127855cdb97459e3e7ad4dfdbb236a4
AwayeCode <98905695+AwayeCode@users.noreply.github.com>
2022-07-03 01:05:43 +0200

    This is DOM manipulation practice
    Several codes were written in my scripts.js file to give the project additional features such button and an event listener to listen to each pokemon upon click. Some styles was also added to css file while calling the button in the scripts.js file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
-let pokemonRepository = (function(){ 
-  let pokemonList = [
+let pokemonRepository = (function () {
+  let repository = [
     {
       name: `Bulbasaur`,
       height: 1.7,
       type: [`grass`, `poison`]
     },
-   
+
     {
       name: `Charizard`,
       height: 0.7,
@@ -17,30 +17,121 @@ let pokemonRepository = (function(){
       height: 1,
       type: [`water`]
     }
-  ]
-    function myLoopFunction(pokemon){
-      document.write(pokemon.name + " " + "is" + " " + pokemon.height + " " + "in height");
-    document.write("<br>");
-    };
-    pokemonList.forEach(myLoopFunction);
+  ];
+  // This is an old code. I left it for me to remember what I wrote earlier...
+  //function myLoopFunction(pokemon) {
+  //document.write(pokemon.name + " " + "is" + " " + pokemon.height + " " + "in height");
+  //document.write("<br>");
+  //};
+  //pokemonList.forEach(myLoopFunction);
 
-    function add(pokemon){
-      pokemonList.push(pokemon);
+  //Adjusted addition starts here &...
+
+  function add(pokemon) {
+
+    repository.push(pokemon);
+  };
+    /*if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      repository.push(pokemon);
+    } else {
+      console.log("pokemon is not correct");
     }
-    function getAll(){
-      return pokemonList;
+  }; */
+  //ends here...
+
+  function getAll() {
+    return repository;
+  };
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  };
+
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector(".pokemon-List");
+    let listpokemon = document.createElement("li");
+    let button = document.createElement(`button`);
+    button.addEventListener(`click`, function (event) {
+      showDetails(pokemon);
+      button.innerText = "pokemon.name";
+      button.classList.add("button-class");
+      listpokemon.appendChild(button);
+      pokemonList.appendChild(listpokemon);
+    });
+  };
+
+  return {
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem,
+    showDetails: showDetails,
+  };
+})();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+  name: `Pikachu`,
+  height: 0.3,
+  type: [`electric`],
+});
+console.log(pokemonRepository.add());
+console.log(pokemonRepository.getAll());
+document.write(pokemonRepository.getAll());
+
+//Old code..
+/*pokemonRepository.getAll().forEach(function(item){
+var size;
+if(item.height > 1){
+  size = "Wow! that`s big!";
+} else {
+  size = "It`s a small pokemon";
+}
+var result;
+  item.types.forEach(function (typeItem) {
+    if(typeItem == `grass`){
+      result = `<span style = "color : green;" >`;
+    } else if(typeItem == `fire`) {
+      result = `<span style = "color : red;" >`;
+    } else if(typeItem == `electric`) {
+      result = `<span style = "color : yellow;" >`;
+    } else if(typeItem == `poison`) {
+      result = `<span style = "color : rgb(106, 42, 106);" >`;
+    } else if(typeItem == `psychic`) {
+      result = `<span style = "color : orange;" >`;
     }
-    return {
-      add : add,
-      getAll : getAll
-      
-    };
-    })()
-  pokemonRepository.add[{
-    name: `Scyther`,
-    type: [`Bug`, `Flying`],
-    height: 4,
-    abilities: [`Swarm`, `Slash`]
-  }];
-  
-  document.write(pokemonRepository.getAll())
+  });
+  document.write(
+    `<div class = "box">` +
+    item.name +
+    "(height: " +
+    item.height +
+    "m" +
+    ")" +
+    "<br>" +
+    size +
+    result +
+    "<br>" +
+    item.types +
+    "<br>" +
+    "</div>"
+  ); */
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+
+  // I can also change the code by placing the below code inside a function called addListItem in the IIFE above.
+
+  /* let pokemonList = document.querySelector(".pokemon-list");
+     let listpokemon = document.createSelector("li");
+     let button = document.createElement(".button");
+     button.innerText = "pokemon.name";
+     button.classList.add("button-class");
+     listpokemon.append(button);
+     pokemonList.appendChild(listpokemon); */
+
+  pokemonRepository.addListItem(pokemon);
+});
