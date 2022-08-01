aca6963f0dcbf10f6eacb9d9aa538ebd332bd0e2
AwayeCode <98905695+AwayeCode@users.noreply.github.com>
2022-08-01 17:07:08 +0200

    inclusion of old style.css, index.html and scripts.js code and
    I returned the button-class to style.css and also the div id 'modalContainer in the index.html files.
    
    In the scripts.js file, the modal-container was selected at the top of the IIFE as a global variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonRepository = (function () {
+  let modalContainer = document.querySelector('#modalContainer');
   let pokemonList = [];
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
@@ -48,16 +49,71 @@ let pokemonRepository = (function () {
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
       item.types = details.types.map(function (typeObject) {
-          return typeObject.type.name;
+        return typeObject.type.name;
       })
     }).catch(function (e) {
       console.error(e);
     });
   };
 
+  function showModal(title, text) {
+    // Clear all existing modal content
+    modalContainer.innerHTML = ' ';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    //To hide the modal
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    let contentElement = document.createElement('p');
+    contentElement.innerText = text;
+
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(contentElement);
+    modalContainer.appendChild(modal);
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove(/*'is-visible'*/);
+  }
+
+  //The Esc-key scenario, then, can be implemented this way, only hiding the modal if it’s actually visible:
+
+  window.addEventListener('keydown', (e) => {
+    let modalContainer = document.querySelector('#modal-container');
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal
+    // We only want to close if the user clicks directly on the overlay
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is a pokémon');
+  });
+
+
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
-      console.log(pokemon);
+      showModal(pokemon);
     });
   };
 
@@ -72,7 +128,7 @@ let pokemonRepository = (function () {
 })();
 
 pokemonRepository.loadList().then(function () {
-  
+
   pokemonRepository.getAll().forEach(function (pokemon) {
     pokemonRepository.addListItem(pokemon);
   });
